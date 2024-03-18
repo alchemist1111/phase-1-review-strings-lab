cbfe149644bea137703ddf1d0a73d28e046cca8d
eugene-emoyo-ojakaa <emoyoeugine@gmail.com>
2024-03-18 18:24:32 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Grace hopper";
+let welcomeMessage = "Welcome to Flatbook, ${Grace hopper}!";
+let  excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${GRACE HOPPER}!";
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
